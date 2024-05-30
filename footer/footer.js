0bb44448486b875d899a9d92132b1520dd5bbe25
mynor <macalderas@scl.edu.gt>
2024-05-30 17:19:35 -0600

    v6

--- footer/footer.js
+++ footer/footer.js
@@ -1,13 +1,27 @@
 let footer_DOM = document.querySelector(".footer");
 
 footer_DOM.innerHTML =  `
-<footer class="footer">
-<ul class="link-list">
-    <li><a href="#"></a></li>
-    <li><a href="#">Samuel</a></li>
-    <li><a href="#">Mynor</a></li>
-    <li><a href="#">Gabriel</a></li>
-</ul>
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq-IciWxLW5lsM2Oco4VluWyfOBSmgukVu6w&s" alt="Imagen ">
+<footer>
+<div class="footer-content">
+    <div class="logo">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq-IciWxLW5lsM2Oco4VluWyfOBSmgukVu6w&s" alt="Logo">
+    </div>
+
+    <div class="abajo_redes-sociales">
+    <a href="#" class="icono-red-social"><i class="fab fa-facebook-f"></i></a>
+    <a href="#" class="icono-red-social"><i class="fab fa-twitter"></i></a>
+    <a href="#" class="icono-red-social"><i class="fab fa-instagram"></i></a>
+    </div>
+        
+    <div class="colaboradores">
+        <h2>Colaboradores</h2>
+        <ul>
+        <li>saajvatz@scl.edu.gt 2</li>
+        <li>lglopez@scl.edu.gt</li>
+        <li>pagiron@scl.edu.g</li>
+        <li>macalderas@scl.edu.gt</li>
+        </ul>
+    </div>
+</div>
 </footer>
 `;
